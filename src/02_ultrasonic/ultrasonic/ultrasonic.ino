4f9641aa5a91c54a3fe382b3c82ddcd44297e01d
Juhong <juhong@rendezvue.ai>
2021-09-16 16:56:45 +0900

    update email

--- src/02_ultrasonic/ultrasonic/ultrasonic.ino
+++ src/02_ultrasonic/ultrasonic/ultrasonic.ino
@@ -3,26 +3,10 @@
 void setup()
 {
   Serial.begin(9600) ;
-  
-  pinMode(6, OUTPUT);    //6 : Trigger
-  pinMode(7, INPUT);    //7 : Echo
 }
 
 void loop()
 {
-  //trigger 발생
-  digitalWrite(6, LOW) ;
-  delayMicroseconds(2) ;
-  digitalWrite(6, HIGH) ;
-  delayMicroseconds(10) ;
-  digitalWrite(6, LOW) ;
-  
-  long value = pulseIn(7, HIGH) ;
-  
-
-  float distance = (float)value * 0.01723 ;
-  
-  Serial.print("Ultra Sonic : ") ;
-  Serial.print(distance) ;
-  Serial.println("cm") ;
+  int analog_value = analogRead(A0) ;
+  Serial.println(analog_value) ;
 }
